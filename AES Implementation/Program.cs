001b11c8dd1d4b55ded4e8f4648cfdf2370ea4f4
Anish Wagle <anish.wagle192@gmail.com>
2022-07-20 09:19:56 +0545

    initial commit

---
+++ AES Implementation/Program.cs
@@ -0,0 +1,22 @@
+﻿using DataEncryption;
+using System.Security.Cryptography;
+
+string original = "Here is some data to encrypt!";
+
+// Create a new instance of the Aes
+// class.  This generates a new key and initialization
+// vector (IV).
+using Aes myAes = Aes.Create();
+
+var cry= new Cryptography();
+var cipher = cry.Encrypt(original,cry.GetPublicKey());
+var message = cry.Decrypt(cipher);
+Console.WriteLine($"Public Key:{cry.GetPublicKey()}\n");
+Console.WriteLine($"Cypher:{cipher}\n");
+Console.WriteLine($"Message:{message}\n");
+// Decrypt the bytes to a string.
+//string roundtrip = Cryptography.DecryptStringFromBytes_Aes(encrypted, myAes.Key, myAes.IV);
+
+//Display the original data and the decrypted data.
+Console.WriteLine("Original:   {0}", original);
+//Console.WriteLine("Round Trip: {0}", roundtrip);
---
+++ DataEncryption/Cryptography.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace DataEncryption
+{
+    public class Cryptography
+    {
+        public RSAParameters PublicKey { get; set; }
+        public RSAParameters PrivateKey { get; set; }
+        public RSA RSA { get; set; }
+
+        public Cryptography()
+        {
+            RSA = GetRSACryptoProvider();
+            PublicKey = RSA.ExportParameters(false); 
+            PrivateKey = RSA.ExportParameters(true);
+            
+        }
+        public string GetPublicKey()
+        {
+            var sw = new StringWriter();
+            var xs = new XmlSerializer(typeof(RSAParameters));
+            xs.Serialize(sw, PublicKey);
+
+            
+            return sw.ToString();
+        }
+
+        public RSAParameters StringToRsa(string key)
+        {
+            byte[] byteArray = Encoding.Unicode.GetBytes(key);
+            //byte[] byteArray = Encoding.ASCII.GetBytes(contents);
+            MemoryStream stream = new MemoryStream(byteArray);
+            var xs = new XmlSerializer(typeof(RSAParameters));
+           return (RSAParameters)xs.Deserialize(stream);
+        }
+        public string Encrypt(string plainText,string key)
+        {
+            RSA.ImportParameters(StringToRsa(key));
+            var plainTextBytes = Encoding.Unicode.GetBytes(plainText);
+            var cipherTextBytes = RSA.Encrypt(plainTextBytes, RSAEncryptionPadding.Pkcs1);
+            var cipherText = Convert.ToBase64String(cipherTextBytes);
+            return cipherText;
+
+
+        }
+
+
+        public string Decrypt(string cypher)
+        {
+            RSA.ImportParameters(PrivateKey);
+            var cypherBytes = Convert.FromBase64String(cypher);
+            var textBytes = RSA.Decrypt(cypherBytes, RSAEncryptionPadding.Pkcs1);
+            var message = Encoding.Unicode.GetString(textBytes);
+            return message;
+
+        }
+
+        private RSA GetRSACryptoProvider()
+        {
+            try
+            {
+                var rsa = RSA.Create();
+                rsa.KeySize = 4096;
+                return rsa;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in GetRSACryptoProvider(): {ex}");
+                return null;
+            }
+        }
+
+    }
+}
